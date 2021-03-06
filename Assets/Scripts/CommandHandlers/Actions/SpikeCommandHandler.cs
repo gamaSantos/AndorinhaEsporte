33bad82097789d5a1dde363bbf5efc102ebd554c
gamaSantos <phpdro@gmail.com>
2021-03-05 23:59:23 -0300

    adjust spike position

--- Assets/Scripts/CommandHandlers/Actions/SpikeCommandHandler.cs
+++ Assets/Scripts/CommandHandlers/Actions/SpikeCommandHandler.cs
@@ -11,18 +11,16 @@ namespace AndorinhaEsporte.CommandHandlers.Actions
             var ball = command.Ball;
             var player = command.Player;
             var isRightSide = player.Position.x > 0;
-            var side = isRightSide ? 4.65f : -4.65f;
+            var side = isRightSide ? 4f : -4f;
 
             var target = new Vector3(side, 0, player.TeamFoward.z * -1);
 
             if (player.IsPassTarget && ball.LandingSpot.HasValue)
             {
                 var newTarget = ball.Trajectory.Points.Where(ballPosition =>
-                        ballPosition.y < (player.SpikeHeight - 0.5f) && ballPosition.y > 2.4f).LastOrDefault();
+                        ballPosition.y < player.SpikeHeight && ballPosition.y > 2.4f).LastOrDefault();
 
                 target = newTarget != default(Vector3) ? newTarget : ball.LandingSpot.Value;
-
-                ball.DrawTrajectory();
             }
 
             if (!player.ArrivedInTarget(target))
@@ -42,7 +40,7 @@ namespace AndorinhaEsporte.CommandHandlers.Actions
                 var ballHorizontalDirection = isRightSide ? -0.5f : 0.5f;
                 var direction = new Vector3(ballHorizontalDirection, 0.1f, player.TeamFoward.z);
 
-                ball.MoveInDirection(direction, 7, player.TeamId);
+                ball.MoveInDirection(direction, 6, player.TeamId);
                 player.RemoveAction(PlayerAction.Spike);
                 player.IsPassTarget = false;
             }
