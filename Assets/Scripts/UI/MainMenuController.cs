ed48550b9ddbd8e45e18a069b3b595df091d9f66
gamaSantos <phpdro@gmail.com>
2021-04-28 19:47:28 -0300

    feat: Add main menu click event

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -1,16 +1,30 @@
+using System;
+using UnityEngine;
 using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
 
-public class MainMenuController
+public class MainMenuController : MonoBehaviour
 {
     // Start is called before the first frame update
+    Button startButton;
+    UIDocument document;
+
     void Start()
     {
-        
+        document = GetComponent<UIDocument>();
+        startButton = document.rootVisualElement.Q<Button>(name: "Play");
+
+        startButton.clicked += PlayButtonClicked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    private Action PlayButtonClicked = () =>
+    {
+        SceneManager.LoadScene("Main", LoadSceneMode.Single);
+    };
 }
