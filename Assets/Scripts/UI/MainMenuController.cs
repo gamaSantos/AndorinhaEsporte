97417caf5a1ede34ae8ff3fd54676775199568ca
gamaSantos <36702012+gamaSantos@users.noreply.github.com>
2021-07-18 15:32:05 -0300

    Fix menu events
    Remove component that breaks default unity event system

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
 using AndorinhaEsporte.Data;
+using AndorinhaEsporte.Inputs;
+using UnityEngine.InputSystem.UI;
 
 namespace AndorinhaEsporte.UI
 {
@@ -13,11 +15,13 @@ namespace AndorinhaEsporte.UI
 
         Button _startButton;
         private TeamSelectController _teamSelectController;
-        private AboutPanelController _aboutController;      
+        private AboutPanelController _aboutController;
+        private AndorinhaUserActions _actions;
 
         void Start()
         {
             var root = GetComponent<UIDocument>().rootVisualElement;
+
 
             BindMainMenu(root);
 
@@ -25,13 +29,26 @@ namespace AndorinhaEsporte.UI
 
             _teamSelectController = new TeamSelectController(root, new TeamRepository());
             _teamSelectController.TeamSelected += OnTeamSelected;
+
+            BindGenericActions();
+
+        }
+
+        private void BindGenericActions()
+        {
+            var inputSystem = GetComponent<InputSystemUIInputModule>();
+            inputSystem.move.action.performed += ctx =>
+            {
+                Debug.Log("move");
+            };
         }
 
         private void BindMainMenu(VisualElement root)
         {
             _startButton = root.Q<Button>(name: "Play");
-            _startButton.clicked += PlayButtonClicked;
 
+            _startButton.clicked += PlayButtonClicked;
+            _startButton.SetEnabled(true);
             root.Q<Button>(name: "About").clicked += () =>
             {
                 _teamSelectController.Hide();
